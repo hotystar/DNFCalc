9d72dc418a644f7458d74a1f5ae1f76e24c23f03
ity <bju11@naver.com>
2016-09-08 18:50:09 +0900

    auto-reload

--- html/app.py
+++ html/app.py
@@ -2,14 +2,10 @@ import random
 from flask import Flask
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-
-def test_print():
-    return """<img src='static/images/%d.png' width='300' height='300'>""" % (random.randrange(1,8))
-
-@app.route("/")
-def home():
-    return test_print()
+import api
 
 if __name__ == '__main__':
-    app.run() 
+    app.run()
