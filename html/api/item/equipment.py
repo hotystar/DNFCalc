c5cd760bc88b604b94acdd8c7331161520c3bc30
ity <bju11@naver.com>
2016-09-09 19:37:17 +0900

    modal dropdown test

--- html/api/item/equipment.py
+++ html/api/item/equipment.py
@@ -1,6 +1,31 @@
+import json
+from app import app
+
+
 class EquipmentItem:
     level = 0
     force = 0
     intel = 0
     
+
+
+
+@app.route('/getitemlist/<string:part>/', methods=['POST'])
+def get_itemlist(part):
+    result = []
     
+    if part == 'weapon':
+        result.append('knife')
+        result.append('sword')
+        result.append('staff')
+        result.append('bow')
+    elif part == 'title':
+        result.append('babo')
+        result.append('hogu')
+        result.append('bs')
+    elif part == 'creature':
+        result.append('togu')
+        result.append('ingyeo')
+    
+    
+    return json.dumps(result)
